3fd5f977d565e05fdf9741de59dc6fd8c3717e5f
Audrey Garcia <audreygrace617@yahoo.com>
2025-07-01 16:26:07 -0700

    Added Cleaned SVI file
    See csv file for cleaned data. See R file for code of cleaning and description of variables.

---
+++ svi_2020.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+
+# Load in Full Data for 2020 SVI in California
+svi_2020_full <- read_csv("svi_2020.csv")
+
+# Clean Data
+# Selected columns for overall svi percentile, as well as the percentile for each of 4 categories
+svi_2020 <- svi_2020_full |>
+  select(c(COUNTY, RPL_THEMES, RPL_THEME1, RPL_THEME2, RPL_THEME3, RPL_THEME4)) |>
+  rename(svi = RPL_THEMES, ses = RPL_THEME1, hhc = RPL_THEME2, rems = RPL_THEME3, htt = RPL_THEME4)
+
+# Note: 
+# svi = social vulnerability index
+# ses = socioeconomic status
+# hhc = household characteristics
+# rems = racial and ethnic minority status
+# htt = housing type / transportation
+
