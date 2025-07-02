b120f48190878d28a22645a62f38d2f8685cf988
genesisxgomez <genesisxgomez@gmail.com>
2025-07-02 15:28:00 -0700

    Update SVI_COVID.R
    There was a mistake in line 3. It was "covid_CA_original" but the actual dataset name is "covid_CA"

--- SVI_COVID.R
+++ SVI_COVID.R
@@ -1,6 +1,6 @@
 covid_county <- read.csv('~/Creative Cloud Files/MSRI-UP/Weekly_United_States_COVID-19_Cases_and_Deaths_by_County_-_ARCHIVED_20250702.csv')
 
-covid_CA_original <- filter(covid_county, state == 'CA')
+covid_CA <- filter(covid_county, state == 'CA')
 
 Covid_CA = subset(covid_CA, select = -c(fips_code, state_fips, cumulative_deaths, New.deaths) )
 
