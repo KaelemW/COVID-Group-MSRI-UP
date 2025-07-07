52772a945741177999ea498298ba1676d45e6858
genesisxgomez <genesisxgomez@gmail.com>
2025-07-07 09:47:48 -0700

    Update SVI_COVID.R
    I added the code to convert character into date format as well as how to filter out the data

--- SVI_COVID.R
+++ SVI_COVID.R
@@ -5,3 +5,12 @@ covid_CA <- filter(covid_county, state == 'CA')
 Covid_CA = subset(covid_CA, select = -c(fips_code, state_fips, cumulative_deaths, New.deaths) )
 
  #write_csv(Covid_CA, 'Covid_CA.csv')
+
+# Convert date column from character to date format
+# We need this in order to filter the data by date 
+Covid_CA <- Covid_CA %>%
+  mutate(date = as.Date(date, format='%m/%d/%Y'))
+
+# Now, we filter out data to year 2021
+ covid_cases_2021 <- filter(Covid_CA, between(date, as.Date('2021-01-01'), 
+                                              as.Date('2021-12-31')))
